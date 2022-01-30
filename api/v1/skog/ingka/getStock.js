d2f8eeede7a0d52bb6c49ea3d42629d3355a6835
Joshua Noakes <joshua.noakes1@icloud.com>
2022-01-30 03:37:13 +0000

    fixed spacing to match files

--- api/v1/skog/ingka/getStock.js
+++ api/v1/skog/ingka/getStock.js
@@ -11,17 +11,17 @@ const lcl = require('cli-color'),
         }
 
         // log lookup
-        console.log(lcl.blue("[Stock Lookup - Info]"), "Looking up stock data for skog in store :",  lcl.yellow(`${storeName} (${buCode})`));
+        console.log(lcl.blue("[Stock Lookup - Info]"), "Looking up stock data for skog in store:",  lcl.yellow(`${storeName} (${buCode})`));
 
         // lookup stock for skog in store
         try {
-            console.log(lcl.green("[Stock Lookup - Success]"), "Found stock data for skog in store :",  lcl.yellow(`${storeName} (${buCode})`));
+            console.log(lcl.green("[Stock Lookup - Success]"), "Found stock data for skog in store:",  lcl.yellow(`${storeName} (${buCode})`));
             return {
                 success: true,
                 stock: await ingka.availability(buCode, '00402813') // 00402813 is code for skog
             }
         } catch (e) {
-            console.log(lcl.red("[Stock Lookup - Error]"), "Failed to get stock data for skog in store :",  lcl.yellow(`${storeName} (${buCode})`));
+            console.log(lcl.red("[Stock Lookup - Error]"), "Failed to get stock data for skog in store:",  lcl.yellow(`${storeName} (${buCode})`));
             return {
                 success: false
             }
