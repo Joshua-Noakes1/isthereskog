54ba9266b1d1bae71bf255eff45ec85a0d65480c
Joshua Noakes <joshua.noakes1@icloud.com>
2022-02-18 23:51:33 +0000

    add chart.js

---
+++ src/components/Card.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Chart from './Chart.svelte'
+</script>
+
+<div class="container">
+  <div class="card">
+    <div class="card-content">
+      <div class="content">
+        <p class="title is-3 has-text-centered">Chart</p>
+        <Chart />
+      </div>
+    </div>
+  </div>
+</div>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,7 @@
+<nav class="navbar is-dark" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="#">
+      <p class="navbar-item title is-4 has-text-light">is there skog?</p>
+    </a>
+  </div>
+</nav>
