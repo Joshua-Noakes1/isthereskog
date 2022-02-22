6bf6465d096bfc3108cec8335a115b9f4c839ec3
Joshua Noakes <joshua.noakes1@icloud.com>
2022-02-22 18:42:51 +0000

    revert form change

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -81,13 +81,11 @@
   }
 </script>
 
-<form on:submit={btnHndl}>
-  <div class="has-text-centered">
-    <p class="title is-4">Enter buCode</p>
-    <input class="input" placeholder="Enter valid buCode" bind:value={buCode} />
-    <div style="margin: 10px 0 10px 0;" />
-    <button type="submit" class="button" on:click={btnHndl}>Check Fat Bear</button>
-  </div>
-</form>
+<div class="has-text-centered">
+  <p class="title is-4">Enter buCode</p>
+  <input class="input" placeholder="Enter valid buCode" bind:value={buCode} />
+  <div style="margin: 10px 0 10px 0;" />
+  <button class="button" on:click={btnHndl}>Check Fat Bear</button>
+</div>
 <div style="margin: 10px 0 10px 0;" />
-<canvas id="myChart" />
+<canvas id="myChart"/>
